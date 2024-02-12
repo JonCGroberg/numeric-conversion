b2e41582618e5d2484dbc7a7ea0c68217a07bc25
Jonathan Groberg <jongroberg@gmail.com>
2024-02-12 17:47:39 -0500

    fixed case errors

--- numeric_conversion.py
+++ numeric_conversion.py
@@ -1,21 +1,26 @@
 def main():
-    selection = prompt_menu()  # ask user to select an option => return the option
-    user_number = prompt_number()  # ask user to enter a number => return the number
-    conversion_result = decode(selection, user_number)  # Decodes the number based on the prompt
+    user_selection = prompt_menu()  # ask user to select an option => return the option
+    user_number = prompt_number()  # ask user to enter a number => return number
+    conversion_result = decode(user_selection, user_number)  # Decodes the number based on the prompt
 
     print(f"Result: {conversion_result}\n")  # prints result & adds new line for next run
     main()  # Run recursively until the user selects 4
 
 
-def decode(selection, user_number):
+def strip(string: str):
+    if string.startswith('0x') or string.startswith("0b"):
+        return string[2:]  # removes the first two characters from the string
+    else:
+        return string
+
+
+def decode(selection: str, user_number: str):
     if int(selection) == 1:
         return hex_string_decode(user_number)
     elif int(selection) == 2:
         return binary_string_decode(user_number)
     elif int(selection) == 3:
         return binary_to_hex(user_number)
-    elif int(selection) == 4:
-        return quit()
 
 
 def prompt_menu():
@@ -25,31 +30,36 @@ def prompt_menu():
     print("2. Decode binary")
     print("3. Convert binary to hexadecimal")
     print("4. Quit")
-    return input("Please enter an option: ")
+    user_input = (input("Please enter an option: "))
+
+    if user_input == "4":
+        print("Goodbye!")
+        quit()
+    elif user_input != "1" and user_input != "2" and user_input != "3":
+        quit()
+    else:
+        return user_input
 
 
 def prompt_number():
     return input("Please enter the numeric string to convert: ")
 
 
-def hex_char_decode(digit):
+def hex_char_decode(digit: str):
     """Decodes a single hexadecimal digit and returns its value."""
     hex_digits = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11,
                   'C': 12, 'D': 13, 'E': 14, 'F': 15}
-    return hex_digits[digit]  # reads the dictionary and returns the corresponding value
+    return hex_digits[digit.upper()]  # reads the dictionary and returns the corresponding value
 
 
-def hex_string_decode(hex):
+def hex_string_decode(hex: str):
     """Decodes an entire hexadecimal string and returns its value."""
-    hex_str = hex
-    index = len(hex) - 1
+    hex_str = strip(hex)
+    index = len(hex_str) - 1
     result = 0
 
-    if hex.startswith('0x') or hex.startswith("0b"):
-        hex_str = str[2:]  # removes the first two characters from the string
-
     for char in hex_str:
-        conversion = hex_char_decode(char.upper())  # conversion result of the uppercase version of the char
+        conversion = hex_char_decode(char)  # conversion result of the uppercase version of the char
         result += conversion * (16 ** index)  # 16 ** index represents the place value as multiples of  16 (hex)
         index -= 1
 
@@ -62,13 +72,13 @@ def hex_string_encode(decimal):
     return result
 
 
-def binary_string_decode(binary):
+def binary_string_decode(binary: str):
     """Decodes a binary string and returns its value."""
-    binary_str = binary
-    index = len(binary) - 1
+    binary_str = strip(binary)
+    index = len(binary_str) - 1
     result = 0
 
-    for char in binary:
+    for char in binary_str:
         result += int(char) * (2 ** index)  # 2 ** index represents the place value as multiples of 2 (binary)
         index -= 1
 
